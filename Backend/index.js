eac1f9c921cad21e9b8314f157a43590f8306fa5
RonitGandhi <ronitgandhi96@gmail.com>
2022-09-09 12:43:47 +0530

    entry point(index.js) configuration

---
+++ Backend/index.js
@@ -0,0 +1,19 @@
+const connectToMango = require("./db");
+const express = require("express");
+
+connectToMango();
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+// Routes
+app.get("/", (req, res) => {
+	res.send("HelloWorld");
+});
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/notes", require("./routes/notes"));
+
+app.listen(port, () => {
+	console.log(`App at http://localhost:${port}`);
+});
