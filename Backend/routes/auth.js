fe51f99eb4300b1cee0b0bc14a96d35039f6a2cc
RonitGandhi <ronitgandhi96@gmail.com>
2022-09-10 17:51:22 +0530

    added async await. and try catch

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -3,8 +3,9 @@ const User = require("../models/User");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
 
+// Post (/api/auth/createuser)
 router.post(
-	"/",
+	"/createuser",
 	[
 		body("name", "Enter a valid name.").isLength({ min: 3 }),
 		body("email", "Enter a valid email.").isEmail(),
@@ -13,24 +14,31 @@ router.post(
 			"Password is too short. It must have atleast 6 character"
 		).isLength({ min: 6 }),
 	],
-	(req, res) => {
+	async (req, res) => {
+		//If there are errors, it will return BAD Request and show the errors.
 		const errors = validationResult(req);
 		if (!errors.isEmpty()) {
 			return res.status(400).json({ errors: errors.array() });
 		}
-		User.create({
-			name: req.body.name,
-			email: req.body.email,
-			password: req.body.password,
-		})
-			.then((user) => res.json(user))
-			.catch((err) => {
-				console.log(err);
-				res.send({
-					error: "Please enter a unique value for email.",
-					message: err.message,
+		// Checks whether user with same email exits or not.
+		try {
+			let user = await User.findOne({ email: req.body.email });
+			if (user) {
+				return res.status(400).json({
+					error: "Sorry a user with this email already exist.",
 				});
+			}
+			user = await User.create({
+				name: req.body.name,
+				email: req.body.email,
+				password: req.body.password,
 			});
+			res.send(req.body);
+			console.log(req.body);
+		} catch (error) {
+			console.error(error.message);
+			res.status(500).send("Some Erorr occured.");
+		}
 	}
 );
 
