8eaf22d13067f8b5d6fa8ae592ac3a672363d5d6
RonitGandhi <ronitgandhi96@gmail.com>
2022-09-14 13:16:25 +0530

    added fetch all notes of current users.

--- Backend/routes/notes.js
+++ Backend/routes/notes.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const Notes = require("../models/Notes");
 const router = express.Router();
+const fetchuser = require("../middleware/fetchuser");
 
-router.get("/", (req, res) => {
-	res.json([]);
+// Route 1: Fetch all notes: GET (/api/notes/fetchallnotes)
+router.get("/fetchallnotes", fetchuser, async (req, res) => {
+	const notes = await Notes.find({ user: req.user.id });
+	res.json(notes);
 });
 
 module.exports = router;
