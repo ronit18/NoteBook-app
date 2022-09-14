3c118805fd579ed52c01eea01370e5c879f0c35b
RonitGandhi <ronitgandhi96@gmail.com>
2022-09-14 16:42:22 +0530

    setup routes through react router dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import About from "./components/About";
+import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+
 const App = () => {
 	return (
 		<>
 			<Navbar />
+			<Routes>
+				<Route extact path="/home" element={<Home />} />
+				<Route extact path="/about" element={<About />} />
+			</Routes>
 		</>
 	);
 };
