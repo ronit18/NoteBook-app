0b968fa01ce2ea0cbb558d633bb1f4857a98f0a2
RonitGandhi <ronitgandhi96@gmail.com>
2022-09-26 18:29:35 +0530

    notes component

--- src/context/notes/NotesState.js
+++ src/context/notes/NotesState.js
@@ -1,14 +1,34 @@
-import noteContext from "./noteContext";
+import NoteContext from "./noteContext";
+import { useState } from "react";
 
-const noteState = (props) => {
-	const state = {
-		name: "ronit",
-	};
+const NoteState = (props) => {
+	const notesInitial = [
+		{
+			_id: "6321a681afeaca0d74813054",
+			user: "6321a638afeaca0d7481304d",
+			title: "this is title",
+			description: "my description2",
+			tag: "personal2",
+			date: "2022-09-14T10:01:37.984Z",
+			__v: 0,
+		},
+		{
+			_id: "632f37a70ddf25ea0adb2b3a",
+			user: "6321a638afeaca0d7481304d",
+			title: "this is title2",
+			description: "my description22222",
+			tag: "personal2",
+			date: "2022-09-24T17:00:23.585Z",
+			__v: 0,
+		},
+	];
+	const [notes, setNotes] = useState(notesInitial);
+
 	return (
-		<noteContext.Provider value={state}>
+		<NoteContext.Provider value={{ notes, setNotes }}>
 			{props.children}
-		</noteContext.Provider>
+		</NoteContext.Provider>
 	);
 };
 
-export default noteState;
+export default NoteState;
