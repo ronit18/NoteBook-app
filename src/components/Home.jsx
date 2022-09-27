acb781e2bc997f0f3fb12453ebc67c260355d223
RonitGandhi <ronitgandhi96@gmail.com>
2022-09-27 15:48:15 +0530

    separate addNote component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,58 +1,10 @@
-import React, { useContext } from "react";
+import React from "react";
 import Notes from "./Notes";
-import noteContext from "../context/notes/noteContext";
 
 const Home = () => {
-	const context = useContext(noteContext);
-	const { mode } = context;
 	return (
 		<>
-			<div className={`text-${mode === "light" ? "dark" : "light"}`}>
-				<h1>Add a Note</h1>
-				<div className="container my-3">
-					<form>
-						<div className="mb-3">
-							<label
-								htmlFor="exampleInputEmail1"
-								className="form-label"
-							></label>
-							<input
-								type="email"
-								className="form-control"
-								id="exampleInputEmail1"
-								aria-describedby="emailHelp"
-							/>
-							<div id="emailHelp" className="form-text"></div>
-						</div>
-						<div className="mb-3">
-							<label
-								htmlFor="exampleInputPassword1"
-								className="form-label"
-							></label>
-							<input
-								type="password"
-								className="form-control"
-								id="exampleInputPassword1"
-							/>
-						</div>
-						<div className="mb-3 form-check">
-							<input
-								type="checkbox"
-								className="form-check-input"
-								id="exampleCheck1"
-							/>
-							<label
-								className="form-check-label"
-								htmlFor="exampleCheck1"
-							></label>
-						</div>
-						<button type="submit" className="btn btn-primary">
-							Submit
-						</button>
-					</form>
-				</div>
-				<Notes />
-			</div>
+			<Notes />
 		</>
 	);
 };
