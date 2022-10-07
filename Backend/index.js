5ff1a6df227f5c1f4462f52e7daf3e5e8c9610eb
RonitGandhi <ronitgandhi96@gmail.com>
2022-10-07 08:45:55 +0530

    added cors package in backend express

--- Backend/index.js
+++ Backend/index.js
@@ -4,7 +4,9 @@ const express = require("express");
 connectToMango();
 const app = express();
 const port = 5000;
+var cors = require("cors");
 
+app.use(cors());
 app.use(express.json());
 
 // Routes
